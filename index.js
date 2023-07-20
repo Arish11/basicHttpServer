b05e72e14d0c53351d038eb0089157d967eb24b8
arish11 <abhishekarya.8596@gmail.com>
2023-07-20 13:32:18 +0530

    using fs module to read an html file and serve to the browser

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const http = require('http')
+const port = 8080
+const fs = require('fs')
 
 function requestHandler(request, response){
    console.log(request.url)
 
-   if(request.url='/'){
-    response.writeHead(200, {'content-type':'text/html'})
-    response.end("<h1>Hello Homie</h1>")
-   }
+//    if(request.url='/'){
+//     response.writeHead(200, {'content-type':'text/html'})
+//     response.end("<h1>Hello Homie</h1>")
+//    }
+
+    fs.readFile('./index.html',(err,data)=>{
+        if(err){
+            return response.end("<h1>Error</h1>")
+        }else{
+            return response.end(data)
+        }
+    })
 }
 
 const server = http.createServer(requestHandler)
-
-const port = 8080
 
 server.listen(8080, function(err){
     if(err){
